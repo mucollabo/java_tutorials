c09ab3aa29ae81064b6cbed50fcd9349f1857c89
Charles An <mucollabo@gmail.com>
2021-02-09 00:48:48 +0900

    study until chap21

---
+++ src/org/opentutorials/javatutorials/object/CalculatorDemo.java
@@ -0,0 +1,8 @@
+package org.opentutorials.javatutorials.object;
+
+public class CalculatorDemo {
+	public static void main(String[] args) {
+		System.out.println(10 + 20);
+		System.out.println(20 + 40);
+	}
+}
