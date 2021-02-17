57dba9d55a16a46984114dc7e07db5fb73191d1f
Charles An <mucollabo@gmail.com>
2021-02-17 21:28:58 +0900

    study until chap28

--- src/org/opentutorials/javatutorials/overriding/example1/CalculatorDemo.java
+++ src/org/opentutorials/javatutorials/overriding/example1/CalculatorDemo.java
@@ -2,41 +2,51 @@ package org.opentutorials.javatutorials.overriding.example1;
 
 class Calculator {
 	int left, right;
+	int third = 0;
 	
 	public void setOperands(int left, int right) {
+		System.out.println("setOperands(int lefg, int right)");
 		this.left = left;
 		this.right = right;
 	}
 	
-	public void sum() {
-		System.out.println(this.left + this.right);
+	public void setOperands(int left, int right, int third) {
+		System.out.println("setOperands(int lefg, int right, int third)");
+		this.setOperands(left, right);
+		this.third = third;
 	}
 	
-	public int avg() {
-		return((this.left + this.right)/2);
+	public void sum() {
+		System.out.println(this.left + this.right + this.third);
+	}
+	
+	public void avg() {
+		System.out.println((this.left + this.right + this.third)/3);
 	}
 }
 
-class SubtractionableCalculator extends Calculator {
-	public void sum() {
-		System.out.println("실행 결과는 " +(this.left + this.right)+"입니다.");
-	}
-	
-	public int avg() {
-		return super.avg();
-	}
-	
-	public void subtract() {
-		System.out.println(this.left - this.right);
-	}
-}
+//class SubtractionableCalculator extends Calculator {
+//	public void sum() {
+//		System.out.println("실행 결과는 " +(this.left + this.right)+"입니다.");
+//	}
+//	
+//	public int avg() {
+//		return super.avg();
+//	}
+//	
+//	public void subtract() {
+//		System.out.println(this.left - this.right);
+//	}
+//}
 
 public class CalculatorDemo {
 	public static void main(String[] args) {
-		SubtractionableCalculator c1 = new SubtractionableCalculator();
+		Calculator c1 = new Calculator();
 		c1.setOperands(10, 20);
 		c1.sum();
-		System.out.println("실행 결과는" + c1.avg());
-		c1.subtract();
+		c1.avg();
+		c1.setOperands(10, 20, 30);
+		c1.sum();
+		c1.avg();
 	}
 }
