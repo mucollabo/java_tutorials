38b6ac93b83fb9e8ae1faec2647dc8d9d22372fa
Charles An <mucollabo@gmail.com>
2021-04-08 15:52:02 +0900

    study until chap39_02

--- src/org/opentutorials/javatutorilas/exception/CalculatorDemo.java
+++ src/org/opentutorials/javatutorilas/exception/CalculatorDemo.java
@@ -4,22 +4,28 @@ class Calculator {
 	int left, right;
 	
 	public void setOperands(int left, int right) {
+		if (right == 0) {
+			throw new IllegalArgumentException("두 번째 인자의 값은 0이 될 수 없습니다.");
+		}
 		this.left = left;
 		this.right = right;
 	}
 	
 	public void divide() {
+		if (this.right == 0) {
+			throw new ArithmeticException("0으로 나누는 것은 허용되지 않습니다.");
+		}
+		
 		try {
 			System.out.print("계산결과는 ");
 			System.out.print(this.left/this.right);
 			System.out.print("입니다.");
 		} catch (Exception e) {
 			System.out.println("\n\ne.getMessage()\n" + e.getMessage());
-//			System.out.println("\n\ne.toString()\n" + e.toString());
-//			System.out.println("\n\ne.printStackTrack()");
-//			e.printStackTrace();
+			System.out.println("\n\ne.toString()\n" + e.toString());
+			System.out.println("\n\ne.printStackTrack()");
+			e.printStackTrace();
 		}
-		System.out.println("David End");
 	}
 }
 
@@ -27,6 +33,10 @@ public class CalculatorDemo {
 	public static void main(String[] args) {
 		Calculator c1 = new Calculator();
 		c1.setOperands(10, 0);
-		c1.divide();
+		try {
+			c1.divide();
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
